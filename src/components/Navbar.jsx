a564872f5a840a24cded6505054c30f9fc58f366
Nathaniel Yee <nathany718@gmail.com>
2025-06-08 22:02:07 -0400

    fix name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> Nathaniel Yee </span>{" "}
+            <span className="text-glow text-foreground"> Nathaniel </span>{" "}
             Yee
           </span>
         </a>
